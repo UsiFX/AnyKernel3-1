4d7c398f2d4acb2866f577c7392ac907e15ef673
Gian <gianpaoloestacio5@gmail.com>
2022-05-25 19:51:27 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Meow🐈 for realme C25 Series
+kernel.string=LiquidKernel💦 for realme C25 Series
 do.devicecheck=1
 do.modules=0
 do.systemless=1
