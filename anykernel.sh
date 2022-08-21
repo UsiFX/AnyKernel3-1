52b8fb04de2ae356139f69b339217c1069b667b1
Gian <gianpaoloestacio5@gmail.com>
2022-08-21 00:49:55 +0000

    Add A10
    Signed-off-by: Gian <gianpaoloestacio5@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,24 +4,24 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Nexus Kernel for realme r5x
+kernel.string=Liquid Kernel for Galaxy A10
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=RMX1911
-device.name2=RMX2030
-device.name3=RMX1925
-device.name4=RMX1927
-device.name5=r5x
+do.cleanuponabort=1
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/by-name/boot;
-is_slot_device=0;
+block=auto;
+is_slot_device=auto;
 ramdisk_compression=auto;
 
 
