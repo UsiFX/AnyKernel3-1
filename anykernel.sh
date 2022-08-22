db571653b326532199131955862ba4e4d08d3bde
eun0115 <gianpaoloestacio5@gmail.com>
2022-08-22 09:48:19 +0800

    update

--- anykernel.sh
+++ anykernel.sh
@@ -4,12 +4,11 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Liquid Kernel for Galaxy A10
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
-do.cleanuponabort=1
+do.cleanuponabort=0
 device.name1=
 device.name2=
 device.name3=
@@ -20,45 +19,34 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=auto;
-is_slot_device=auto;
+block=/dev/block/platform/13500000.dwmmc0/by-name/boot;
+dtboblock=/dev/block/platform/13500000.dwmmc0/by-name/dtbo;
+is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+## Trim partitions
+$bin/busybox fstrim -v /data;
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
-## AnyKernel install
+## AnyKernel boot install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
-
 write_boot;
-## end install
+## end boot install
 
+# Vendor boot
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+## AnyKernel vendor_boot install
+#split_boot;
+
+#flash_boot;
+## end vendor_boot install
